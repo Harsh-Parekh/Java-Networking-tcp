8bd1f74bd066024110009b31c324a6ccf39cb01e
Harsh Parekh <58972156+Harsh-Parekh@users.noreply.github.com>
2020-03-02 11:05:38 +0530

    Create ReverseServer.java
    Client send string and server reverse it and send to client again

---
+++ Server-Client/ReverseServer.java
@@ -0,0 +1,27 @@
+package Networking;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server
+{
+    public static void main(String args[]) throws Exception {
+
+        ServerSocket ss = new ServerSocket(3333);
+        Socket s = ss.accept();
+
+        DataInputStream dis = new DataInputStream(s.getInputStream());
+        DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+
+        String clientstring = dis.readUTF();
+        String reverse = "";
+
+        for (int i = clientstring.length() - 1; i >= 0; i--)
+        {
+            reverse=reverse+clientstring.charAt(i);
+        }
+	dos.writeUTF(reverse);
+    }
+}
